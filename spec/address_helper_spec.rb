9efe1f926572042a2b86a21a0dfb026dee852b6d
Chung-Yi Chi <chung-yi@artsymail.com>
2021-12-29 14:58:59 -0500

    feat: add a list of countries with VAT

--- spec/address_helper_spec.rb
+++ spec/address_helper_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe AddressHelper do
     expect(AddressHelper::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'has a list of countries with VAT' do
+    expect(AddressHelper::COUNTRIES_WITH_VAT).not_to be_empty
   end
 end
