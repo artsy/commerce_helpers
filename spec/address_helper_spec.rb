f94d3bebb86c5089b23993b49786f2d4081bd06d
Chung-Yi Chi <chung-yi@artsymail.com>
2022-02-22 16:27:14 +0800

    Expand the list of countries requiring VAT (#5)
    * feat: add Romania, Hungary, Malta, Bulgaria, and United Arab Emirates to VAT countries
    
    * test: use country name to verify the VAT country list in specs

--- spec/address_helper_spec.rb
+++ spec/address_helper_spec.rb
@@ -1,12 +1,48 @@
 # frozen_string_literal: true
 
+require 'countries'
+
 RSpec.describe AddressHelper do
   it 'has a version number' do
     expect(AddressHelper::VERSION).not_to be nil
   end
 
   it 'has a list of countries requiring VAT' do
-    expect(AddressHelper::COUNTRIES_REQUIRING_VAT).not_to be_empty
+    expect(AddressHelper::COUNTRIES_REQUIRING_VAT).to match_array(
+      [
+        'Austria',
+        'Belgium',
+        'Bulgaria',
+        'Czechia',
+        'Denmark',
+        'Estonia',
+        'Finland',
+        'France',
+        'Germany',
+        'Greece',
+        'Hungary',
+        'Ireland',
+        'Italy',
+        'Latvia',
+        'Lithuania',
+        'Luxembourg',
+        'Malta',
+        'Netherlands',
+        'Norway',
+        'Poland',
+        'Portugal',
+        'Romania',
+        'Slovakia',
+        'Slovenia',
+        'Spain',
+        'Sweden',
+        'Switzerland',
+        'United Arab Emirates',
+        'United Kingdom of Great Britain and Northern Ireland'
+      ].map do |name|
+        ISO3166::Country.find_country_by_iso_short_name(name).alpha2
+      end
+    )
   end
 
   it 'has a list of countries in continental Europe' do
