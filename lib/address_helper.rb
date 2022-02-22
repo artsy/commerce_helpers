3e5ec7a3afd87617e19baf535d6b9a1f59ae727b
Chung-Yi Chi <chung-yi@artsymail.com>
2022-02-22 14:46:03 +0800

    feat: add Romania, Hungary, Malta, Bulgaria, and United Arab Emirates to VAT countries

--- lib/address_helper.rb
+++ lib/address_helper.rb
@@ -3,7 +3,8 @@
 require 'address_helper/version'
 
 module AddressHelper
-  COUNTRIES_REQUIRING_VAT = %w[AT BE CH CZ DE DK EE ES FI FR GB GR IE IT LT LU LV NL NO PL PT SE SI SK].freeze
+  COUNTRIES_REQUIRING_VAT = %w[AE AT BE BG CH CZ DE DK EE ES FI FR GB GR HU IE IT LT LU LV MT NL NO PL PT RO SE SI
+                               SK].freeze
   COUNTRIES_IN_CONTINENTAL_EUROPE = %w[AD AM AT AZ BA BE BG BY CH CY CZ DE DK EE ES FI FR GE HR HU IT KZ LI LT LU LV MC
                                        MD ME MK NL NO PL PT RO RS RU SE SI SK SM TR UA VA].freeze
 end
