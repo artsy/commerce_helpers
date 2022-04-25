d27f8a1f69a829be6760c80b1b9b64648d9445ca
Fladson Gomes <fladsonthiago@gmail.com>
2022-04-25 15:21:39 +0200

    feat: add Mexico to VAT countries

--- lib/address_helper.rb
+++ lib/address_helper.rb
@@ -3,7 +3,7 @@
 require 'address_helper/version'
 
 module AddressHelper
-  COUNTRIES_REQUIRING_VAT = %w[AE AT BE BG CH CZ DE DK EE ES FI FR GB GR HU IE IT LT LU LV MT NL NO PL PT RO SE SI
+  COUNTRIES_REQUIRING_VAT = %w[AE AT BE BG CH CZ DE DK EE ES FI FR GB GR HU IE IT LT LU LV MT MX NL NO PL PT RO SE SI
                                SK].freeze
   COUNTRIES_IN_CONTINENTAL_EUROPE = %w[AD AM AT AZ BA BE BG BY CH CY CZ DE DK EE ES FI FR GE HR HU IT KZ LI LT LU LV MC
                                        MD ME MK NL NO PL PT RO RS RU SE SI SK SM TR UA VA].freeze
